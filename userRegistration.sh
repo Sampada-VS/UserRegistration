9a67850a0657852ec4b64bdb19cab504e7a5a64c
Sampada-VS <samssvv1@gmail.com>
2020-08-12 00:44:54 +0530

    Adding last name validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -8,3 +8,12 @@ then
 else
 	echo "First name should start with capital letter and need min 3 characters."
 fi
+
+read -p "Enter Last name : " last_name
+if [[ $last_name =~ ^[[:upper:]]{1}[[:lower:]]{2,}$ ]]
+then
+	echo "Valid"
+else
+	echo "Last name should start with capital letter and need min 3 characters."
+fi
+
