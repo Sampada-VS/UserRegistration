cc41af0e653e347439e2cbbd97900da31248f308
Sampada-VS <samssvv1@gmail.com>
2020-08-12 00:34:31 +0530

    Adding first name validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -x
 
 echo "'User Registration form'"
+read -p "Enter First name : " first_name
+if [[ $first_name =~ ^[[:upper:]]{1}[[:lower:]]{2,}$ ]]
+then
+	echo "Valid"
+else
+	echo "First name should start with capital letter and need min 3 characters."
+fi
