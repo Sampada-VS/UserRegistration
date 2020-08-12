b1fe5827aabf881a42231a0074e2b4717bf018fe
Sampada-VS <samssvv1@gmail.com>
2020-08-12 21:04:57 +0530

    Email id validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 
 echo "'User Registration form'"
-read -p "Enter Last name : " last_name
-if [[ $last_name =~ ^[[:upper:]]{1}[[:lower:]]{2,}$ ]]
+read -p "Enter Email-id : " email_id
+if [[ $email_id =~ ^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$ ]]
 then
 	echo "Valid"
 else
-	echo "Last name should start with capital letter and need min 3 characters."
+	echo "Invalid email-id"
 fi
 
