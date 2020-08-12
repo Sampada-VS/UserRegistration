e0d3bb0f36ca9649d4237e757b8d72a3a5f9c663
Sampada-VS <samssvv1@gmail.com>
2020-08-12 22:41:22 +0530

    Mobile number validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -6,6 +6,6 @@ if [[ $mob_num =~ ^[1-9]{2,3}[" "][7-9]{1}[0-9]{9}$ ]]
 then
 	echo "Valid"
 else
-	echo "Invalid mobile number"
+	echo "Mobile number should contain country code follow by space and 10 digit number."
 fi
 
