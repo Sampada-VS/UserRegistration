bf6937a21a7024f64a60f9ac67523295a0937b35
Sampada-VS <samssvv1@gmail.com>
2020-08-12 23:15:17 +0530

    Exactly 1 special character in password

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,10 +2,11 @@
 
 echo "'User Registration form'"
 read -p "Enter password : " password
-if [[ ${#password} -ge 8 && $password == *[[:upper:]]* && $password == *[0-9]* ]]
+num_sp_char=$(($(tr -d [[:alnum:]] <<< $password | wc -m )-1))
+if [[ ${#password} -ge 8 && $password == *[[:upper:]]* && $password == *[0-9]* && $num_sp_char -eq 1 && $password != *[" "]* ]]
 then
 	echo "Valid"
 else
-	echo "Password should contain min 8 char, atleast one uppercase and number."
+	echo "Password should contain min 8 char, atleast one uppercase and number, exactly 1 special char with no spaces."
 fi
 
