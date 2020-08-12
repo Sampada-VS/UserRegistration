5ae4f65038b27366838b2243d9f786a77c5fcedf
Sampada-VS <samssvv1@gmail.com>
2020-08-12 22:32:06 +0530

    Mobile number validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 
 echo "'User Registration form'"
-read -p "Enter Email-id : " email_id
-if [[ $email_id =~ ^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$ ]]
+read -p "Enter mobile number : " mob_num
+if [[ $mob_num =~ ^[1-9]{2,3}[" "][7-9]{1}[0-9]{9}$ ]]
 then
 	echo "Valid"
 else
-	echo "Invalid email-id"
+	echo "Invalid mobile number"
 fi
 
